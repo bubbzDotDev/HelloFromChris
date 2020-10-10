52085487099ebbd97f78b50045daec7b8e7280a8
Yippie_Calle <cal12001@byui.edu>
2020-10-09 19:59:40 -0600

    Finishing step 6

--- src/blount/Main.java
+++ src/blount/Main.java
@@ -3,5 +3,6 @@ package blount;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Chris");
+        System.out.println("Hello Chris, my name is Bryan.");
     }
 }
